1faf7cb9d2d5c3ad0ba10985d17ffa54dee3f380
Shastraprathista Trust <shastraprathista@gmail.com>
2026-05-02 20:19:46 +0530

    Update profile invoice

--- frontend/js/profile/profile-invoice.js
+++ frontend/js/profile/profile-invoice.js
@@ -488,7 +488,7 @@ class ProfileInvoice {
         doc.setTextColor(0, 0, 255);
         doc.text('"RAMCO DHARMIKA SEVA"', pageWidth / 2, currentY, { align: 'center' });
         
-        currentY += 16;
+        currentY += 20;
         
         // Invoice details
         doc.setFont('helvetica', 'normal');
